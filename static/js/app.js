c2e0627202b9d7d863385823492c5db5c9afd742
bwfitzpa <137216561+bwfitzpa@users.noreply.github.com>
2024-01-29 20:10:45 -0500

    app

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,11 @@
+//Reading in Japan.Travel.pref_lat_lng.json
+const pref_lat_lng = "data/Japan_Travel_lat_lng";
+
+// Fetch the JSON data and console log it
+d3.json(pref_lat_lng).then(function(data));
+
+
+document.addEventListener("")
 let myMap = L.map("map", {
     center: [36, 137],
     zoom: 5
